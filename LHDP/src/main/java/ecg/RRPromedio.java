9e03b34453938d0179df12e249ddf819ea80566e
francisco-espin-gil-um-es <francisco.esping@um.es>
2025-11-23 16:34:52 +0100

    Fase3 (entrega)

---
+++ LHDP/src/main/java/ecg/RRPromedio.java
@@ -0,0 +1,22 @@
+package ecg;
+
+/**
+ * Hecho derivado: Intervalo RR promedio del ECG
+ * Usado para detectar prematuridad en CVP (Contracciones Ventriculares Prematuras)
+ */
+public class RRPromedio {
+    private double valor;
+
+    public RRPromedio(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "RRPromedio [valor=" + String.format("%.2f", valor) + " ms]";
+    }
+}
